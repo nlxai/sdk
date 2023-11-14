8122864ff705617ca236fffc96e6341089c30388
Peter Szerzo <szerzo.peter@gmail.com>
2023-11-14 11:35:29 +0100

    Fix build

--- packages/website/src/constants.ts
+++ packages/website/src/constants.ts
@@ -3,17 +3,13 @@ const version = "0.0.1";
 export const umdScriptTags = {
   chatCore: `https://unpkg.com/@nlxai/chat-core@${version}/lib/umd/index.js`,
   chatWidget: `https://unpkg.com/@nlxai/chat-widget@${version}/lib/umd/index.js`,
-  voiceCompass: `https://unpkg.com/@nlxai/voice-compass@${version}/lib/umd/index.js`
+  voiceCompass: `https://unpkg.com/@nlxai/voice-compass@${version}/lib/umd/index.js`,
 };
 
 export const packageUrls = {
-  chatCore: "https://github.com/nlxai/chat-sdk/tree/master/packages/chat-core",
-  chatReact:
-    "https://github.com/nlxai/chat-sdk/tree/master/packages/chat-react",
-  chatPreact:
-    "https://github.com/nlxai/chat-sdk/tree/master/packages/chat-preact",
-  chatWidget:
-    "https://github.com/nlxai/chat-sdk/tree/master/packages/chat-widget",
-  voiceCompass:
-    "https://github.com/nlxai/chat-sdk/tree/master/packages/voice-compass"
+  chatCore: "https://github.com/nlxai/sdk/tree/main/packages/chat-core",
+  chatReact: "https://github.com/nlxai/sdk/tree/main/packages/chat-react",
+  chatPreact: "https://github.com/nlxai/sdk/tree/main/packages/chat-preact",
+  chatWidget: "https://github.com/nlxai/sdk/tree/main/packages/chat-widget",
+  voiceCompass: "https://github.com/nlxai/sdk/tree/main/packages/voice-compass",
 };
