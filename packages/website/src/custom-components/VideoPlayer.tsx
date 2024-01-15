39f3f6a4c00cbede18da9962c43598f97bfab119
Vignesh Mohankumar <me@vig.xyz>
2024-01-15 13:44:47 -0500

    video component

---
+++ packages/website/src/custom-components/VideoPlayer.tsx
@@ -0,0 +1,20 @@
+import React, { type FC } from "react";
+
+interface VideoPlayerProps {
+  videoId: string;
+}
+
+const VideoPlayer: FC<VideoPlayerProps> = ({ videoId }) => {
+  const videoSrc = `https://www.youtube.com/embed/${videoId}`;
+  return (
+    <iframe
+      width="380"
+      height="213"
+      src={videoSrc}
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    ></iframe>
+  );
+};
+
+export default VideoPlayer;
