4013a8e2c521ef9761f14f065e55c60331d80adf
Michael Glass <me@mike.is>
2024-04-08 15:36:18 +0200

    add docs generating scripts, tools

---
+++ packages/chat-core/typedoc.cjs
@@ -0,0 +1,12 @@
+/** @type {import('typedoc').TypeDocOptions} */
+module.exports = {
+  sort: ["source-order", "kind", "instance-first", "alphabetical"],
+  entryPoints: ["./src/index.ts"],
+  out: "docs",
+  validation: { notExported: true, invalidLink: true, notDocumented: true },
+  treatWarningsAsErrors: true,
+  treatValidationWarningsAsErrors: true,
+  categoryOrder: ["Setup", "Client"],
+  // plugin: ["typedoc-plugin-markdown"],
+  readme: "none",
+};
