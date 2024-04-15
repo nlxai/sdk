dc9112682a0534df5a71353d75a86b255bc28d3a
Michael Glass <me@mike.is>
2024-04-15 15:39:19 +0200

    better docs rendering

--- packages/chat-core/typedoc.cjs
+++ packages/chat-core/typedoc.cjs
@@ -1,16 +1,14 @@
-// @type {import('typedoc').TypeDocOptions}
+/** @type { import('typedoc').TypeDocOptions & { hideInPageTOC?: boolean, hideBreadcrumbs?: boolean} } */
 module.exports = {
-  sort: ["source-order", "kind", "instance-first", "alphabetical"],
-  entryPoints: ["./src/index.ts"],
-  out: "docs",
-  validation: {
-    notExported: true,
-    invalidLink: true,
-    // notDocumented: true,
-  },
-  treatWarningsAsErrors: true,
-  treatValidationWarningsAsErrors: true,
   categoryOrder: ["Setup", "Client"],
+  entryPoints: ["./src/index.ts"],
+  hideBreadcrumbs: true,
+  hideInPageTOC: true,
+  out: "docs",
   plugin: ["typedoc-plugin-markdown"],
   readme: "none",
+  sort: ["source-order", "kind", "instance-first", "alphabetical"],
+  treatValidationWarningsAsErrors: true,
+  treatWarningsAsErrors: true,
+  validation: { notExported: true, invalidLink: true, notDocumented: true },
 };
