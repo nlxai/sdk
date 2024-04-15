1f71cb18c31245f4f0577c738b67e1522a5c8b80
Michael Glass <me@mike.is>
2024-04-15 15:18:00 +0200

    wip

--- packages/chat-core/src/index.ts
+++ packages/chat-core/src/index.ts
@@ -4,510 +4,15 @@ import { equals, adjust, omit } from "ramda";
 import { v4 as uuid } from "uuid";
 import packageJson from "../package.json";
 
-// Bot response
-
 /**
- * Context to send back to the bot, for usage later in the intent.
+ * Call this to create a conversation handler.
+ *
+ * @param config -
+ * @returns The {@link ConversationHandler} is a bundle of functions to interact with the conversation.
  */
-export type Context = Record<string, any>;
-
-/**
- * Values to fill an intent's [attached slots](https://docs.studio.nlx.ai/intents/documentation-intents/intents-attached-slots).
- *
- * An array of `SlotValue` objects is equivalent to a {@link SlotsRecord}.
- */
-export interface SlotValue {
-  /**
-   * The attached slot's name
-   */
-  slotId: string;
-  /**
-   * Usually this will be a discrete value matching the slots's [type](https://docs.studio.nlx.ai/slots/documentation-slots/slots-values#system-slots).
-   * for custom slots, this can optionally be the value's ID.
-   */
-  value: any;
-}
-
-/**
- * Values to fill an intent's [attached slots](https://docs.studio.nlx.ai/intents/documentation-intents/intents-attached-slots).
- *
- * `SlotRecord` Keys are the attached slot's name
- *
- * `SlotRecord` Values are usually a discrete value matching the slots's [type](https://docs.studio.nlx.ai/slots/documentation-slots/slots-values#system-slots).
- * for custom slots, this can optionally be the value's ID.
- *
- * A `SlotsRecord` is equivalent to an array of {@link SlotValue} objects.
- */
-export type SlotsRecord = Record<string, any>;
-
-/**
- * Values to fill an intent's [attached slots](https://docs.studio.nlx.ai/intents/documentation-intents/intents-attached-slots).
- *
- * Supports either a {@link SlotsRecord} or an array of {@link SlotValue} objects
- */
-export type SlotsRecordOrArray = SlotsRecord | SlotValue[];
-
-/**
- * A message from the bot
- *
- * See also:
- * - {@link UserResponse}
- * - {@link FailureMessage}
- * - {@link Response}
- */
-export interface BotResponse {
-  /**
-   * The type of the response is `"bot"` for bot and `"user"` for user.
-   */
-  type: "bot";
-  /**
-   * When the response was received
-   */
-  receivedAt: Time;
-  /**
-   * The payload of the response
-   */
-  payload: BotResponsePayload;
-}
-
-/**
- *
- */
-export interface BotResponsePayload {
-  /**
-   *
-   */
-  expirationTimestamp?: number;
-  /**
-   *
-   */
-  conversationId?: string;
-  /**
-   *
-   */
-  messages: BotMessage[];
-  /**
-   *
-   */
-  metadata?: BotResponseMetadata;
-  /**
-   *
-   */
-  payload?: string;
-  /**
-   *
-   */
-  modalities?: Record<string, any>;
-  /**
-   *
-   */
-  context?: Context;
-}
-
-/**
- *
- */
-export interface BotResponseMetadata {
-  /**
-   *
-   */
-  intentId?: string;
-  /**
-   *
-   */
-  escalation?: boolean;
-  /**
-   *
-   */
-  frustration?: boolean;
-  /**
-   *
-   */
-  incomprehension?: boolean;
-  /**
-   *
-   */
-  hasPendingDataRequest?: boolean;
-}
-
-/**
- *
- */
-export interface BotMessage {
-  /**
-   *
-   */
-  messageId?: string;
-  /**
-   *
-   */
-  nodeId?: string;
-  /**
-   *
-   */
-  text: string;
-  /**
-   *
-   */
-  choices: Choice[];
-  /**
-   *
-   */
-  selectedChoiceId?: string;
-}
-
-/**
- *
- */
-export interface Choice {
-  /**
-   *
-   */
-  choiceId: string;
-  /**
-   *
-   */
-  choiceText: string;
-  /**
-   *
-   */
-  choicePayload?: string;
-}
-
-// User message
-
-/**
- *
- */
-export interface UserResponse {
-  /**
-   * The type of the response is `"bot"` for bot and `"user"` for user.
-   */
-  type: "user";
-  /**
-   *
-   */
-  receivedAt: Time;
-  /**
-   *
-   */
-  payload: UserResponsePayload;
-}
-
-/**
- *
- */
-export type UserResponsePayload =
-  | {
-      /**
-       *
-       */
-      type: "text";
-      /**
-       *
-       */
-      text: string;
-      /**
-       *
-       */
-      context?: Context;
-    }
-  | {
-      /**
-       *
-       */
-      type: "choice";
-      /**
-       *
-       */
-      choiceId: string;
-      /**
-       *
-       */
-      context?: Context;
-    }
-  | ({
-      /**
-       *
-       */
-      type: "structured";
-      /**
-       *
-       */
-      context?: Context;
-    } & StructuredRequest);
-
-// Failure message
-
-/**
- *
- */
-export interface FailureMessage {
-  /**
-   *
-   */
-  type: "failure";
-  /**
-   *
-   */
-  payload: {
-    /**
-     *
-     */
-    text: string;
-  };
-  /**
-   *
-   */
-  receivedAt: Time;
-}
-
-/**
- *
- */
-export type Response = BotResponse | UserResponse | FailureMessage;
-
-/**
- *
- */
-export type Time = number;
-
-// Config and state
-
-/**
- *
- */
-export type Environment = "production" | "development";
-
-/**
- *
- */
-export interface Config {
-  /**
-   *
-   */
-  botUrl: string;
-  /**
-   *
-   */
-  conversationId?: string;
-  /**
-   *
-   */
-  userId?: string;
-  /**
-   *
-   */
-  responses?: Response[];
-  /**
-   *
-   */
-  failureMessage?: string;
-  /**
-   *
-   */
-  environment?: Environment;
-  /**
-   *
-   */
-  headers: Record<string, string> & { "nlx-api-key": string };
-  /**
-   *
-   */
-  languageCode: string;
-  // Experimental settings
-  /**
-   *
-   */
-  experimental?: {
-    // Simulate alternative channel types
-    /**
-     *
-     */
-    channelType?: string;
-    // Prevent the `languageCode` parameter to be appended to the bot URL - used in special deployment environments such as the sandbox chat inside Dialog Studio
-    /**
-     *
-     */
-    completeBotUrl?: boolean;
-  };
-}
-
-const welcomeIntent = "NLX.Welcome";
-
-const defaultFailureMessage = "We encountered an issue. Please try again soon.";
-
-/**
- *
- */
-export type State = Response[];
-
-const normalizeSlots = (
-  slotsRecordOrArray: SlotsRecordOrArray,
-): SlotValue[] => {
-  if (Array.isArray(slotsRecordOrArray)) {
-    return slotsRecordOrArray;
-  }
-  return Object.entries(slotsRecordOrArray).map(([key, value]) => ({
-    slotId: key,
-    value,
-  }));
-};
-
-/**
- *
- */
-export interface StructuredRequest {
-  /**
-   *
-   */
-  choiceId?: string;
-  /**
-   *
-   */
-  intentId?: string;
-  /**
-   *
-   */
-  nodeId?: string;
-  /**
-   *
-   */
-  slots?: SlotsRecordOrArray;
-  /**
-   *
-   */
-  poll?: boolean;
-}
-
-interface BotRequest {
-  conversationId?: string;
-  userId?: string;
-  context?: Context;
-  request: {
-    unstructured?: {
-      text: string;
-    };
-    structured?: StructuredRequest & { slots?: SlotValue[] };
-  };
-}
-
-/**
- *
- */
-export interface ChoiceRequestMetadata {
-  /**
-   *
-   */
-  responseIndex?: number;
-  /**
-   *
-   */
-  messageIndex?: number;
-  /**
-   *
-   */
-  nodeId?: string;
-}
-
-/**
- *
- */
-export interface ConversationHandler {
-  /**
-   *
-   */
-  sendText: (text: string, context?: Context) => void;
-  /**
-   *
-   */
-  sendSlots: (slots: SlotsRecordOrArray, context?: Context) => void;
-  /**
-   *
-   */
-  sendChoice: (
-    choiceId: string,
-    context?: Context,
-    metadata?: ChoiceRequestMetadata,
-  ) => void;
-  /**
-   *
-   */
-  sendWelcomeIntent: (context?: Context) => void;
-  /**
-   *
-   */
-  sendIntent: (intentId: string, context?: Context) => void;
-  /**
-   *
-   */
-  sendStructured: (request: StructuredRequest, context?: Context) => void;
-  /**
-   *
-   */
-  subscribe: (subscriber: Subscriber) => () => void;
-  /**
-   *
-   */
-  unsubscribe: (subscriber: Subscriber) => void;
-  /**
-   *
-   */
-  unsubscribeAll: () => void;
-  /**
-   *
-   */
-  currentConversationId: () => string | undefined;
-  /**
-   *
-   */
-  reset: (options?: {
-    /**
-     *
-     */
-    clearResponses?: boolean;
-  }) => void;
-  /**
-   *
-   */
-  destroy: () => void;
-}
-
-interface InternalState {
-  responses: Response[];
-  conversationId: string;
-  userId?: string;
-}
-
-const fromInternal = (internalState: InternalState): State =>
-  internalState.responses;
-
-const safeJsonParse = (val: string): any => {
-  try {
-    const json = JSON.parse(val);
-    return json;
-  } catch (_err) {
-    return null;
-  }
-};
-
-/**
- *
- */
-export type Subscriber = (response: Response[], newResponse?: Response) => void;
-
-// Helper method to decide when a conversation needs to be re-initialized (e.g. bot URL change)
-export const shouldReinitialize = (
-  config1: Config,
-  config2: Config,
-): boolean => {
-  return !equals(
-    omit(["failureMessage"], config1),
-    omit(["failureMessage"], config2),
-  );
-};
-
-export const createConversation = (config: Config): ConversationHandler => {
+export default function createConversation(
+  config: Config,
+): ConversationHandler {
   let socket: ReconnectingWebSocket | undefined;
 
   // Check if the bot URL has a language code appended to it
@@ -874,14 +379,539 @@ export const createConversation = (config: Config): ConversationHandler => {
       }
     },
   };
+}
+
+/**
+ *
+ */
+export interface ConversationHandler {
+  /**
+   *
+   */
+  sendText: (text: string, context?: Context) => void;
+  /**
+   *
+   */
+  sendSlots: (slots: SlotsRecordOrArray, context?: Context) => void;
+  /**
+   *
+   */
+  sendChoice: (
+    choiceId: string,
+    context?: Context,
+    metadata?: ChoiceRequestMetadata,
+  ) => void;
+  /**
+   *
+   */
+  sendWelcomeIntent: (context?: Context) => void;
+  /**
+   *
+   */
+  sendIntent: (intentId: string, context?: Context) => void;
+  /**
+   *
+   */
+  sendStructured: (request: StructuredRequest, context?: Context) => void;
+  /**
+   *
+   */
+  subscribe: (subscriber: Subscriber) => () => void;
+  /**
+   *
+   */
+  unsubscribe: (subscriber: Subscriber) => void;
+  /**
+   *
+   */
+  unsubscribeAll: () => void;
+  /**
+   *
+   */
+  currentConversationId: () => string | undefined;
+  /**
+   *
+   */
+  reset: (options?: {
+    /**
+     *
+     */
+    clearResponses?: boolean;
+  }) => void;
+  /**
+   *
+   */
+  destroy: () => void;
+}
+
+/**
+ * Context to send back to the bot, for usage later in the intent.
+ */
+export type Context = Record<string, any>;
+
+/**
+ * Values to fill an intent's [attached slots](https://docs.studio.nlx.ai/intents/documentation-intents/intents-attached-slots).
+ *
+ * An array of `SlotValue` objects is equivalent to a {@link SlotsRecord}.
+ */
+export interface SlotValue {
+  /**
+   * The attached slot's name
+   */
+  slotId: string;
+  /**
+   * Usually this will be a discrete value matching the slots's [type](https://docs.studio.nlx.ai/slots/documentation-slots/slots-values#system-slots).
+   * for custom slots, this can optionally be the value's ID.
+   */
+  value: any;
+}
+
+/**
+ * Values to fill an intent's [attached slots](https://docs.studio.nlx.ai/intents/documentation-intents/intents-attached-slots).
+ *
+ * `SlotRecord` Keys are the attached slot's name
+ *
+ * `SlotRecord` Values are usually a discrete value matching the slots's [type](https://docs.studio.nlx.ai/slots/documentation-slots/slots-values#system-slots).
+ * for custom slots, this can optionally be the value's ID.
+ *
+ * A `SlotsRecord` is equivalent to an array of {@link SlotValue} objects.
+ */
+export type SlotsRecord = Record<string, any>;
+
+/**
+ * Values to fill an intent's [attached slots](https://docs.studio.nlx.ai/intents/documentation-intents/intents-attached-slots).
+ *
+ * Supports either a {@link SlotsRecord} or an array of {@link SlotValue} objects
+ */
+export type SlotsRecordOrArray = SlotsRecord | SlotValue[];
+
+/**
+ * A message from the bot
+ *
+ * See also:
+ * - {@link UserResponse}
+ * - {@link FailureMessage}
+ * - {@link Response}
+ */
+export interface BotResponse {
+  /**
+   * The type of the response is `"bot"` for bot and `"user"` for user.
+   */
+  type: "bot";
+  /**
+   * When the response was received
+   */
+  receivedAt: Time;
+  /**
+   * The payload of the response
+   */
+  payload: BotResponsePayload;
+}
+
+/**
+ * The response when the bot sends a message.
+ */
+export interface BotResponsePayload {
+  /**
+   * If there isn't some interaction by this time, the conversation will expire.
+   */
+  expirationTimestamp?: number;
+  /**
+   * The active conversation ID. If this is null, a new conversation will be started.
+   */
+  conversationId?: string;
+  /**
+   *
+   */
+  messages: BotMessage[];
+  /**
+   *
+   */
+  metadata?: BotResponseMetadata;
+  /**
+   *
+   */
+  payload?: string;
+  /**
+   *
+   */
+  modalities?: Record<string, any>;
+  /**
+   * Any context the bot knows about the current conversation.
+   */
+  context?: Context;
+}
+
+/**
+ *
+ */
+export interface BotResponseMetadata {
+  /**
+   *
+   */
+  intentId?: string;
+  /**
+   *
+   */
+  escalation?: boolean;
+  /**
+   *
+   */
+  frustration?: boolean;
+  /**
+   *
+   */
+  incomprehension?: boolean;
+  /**
+   *
+   */
+  hasPendingDataRequest?: boolean;
+}
+
+/**
+ *
+ */
+export interface BotMessage {
+  /**
+   *
+   */
+  messageId?: string;
+  /**
+   *
+   */
+  nodeId?: string;
+  /**
+   *
+   */
+  text: string;
+  /**
+   *
+   */
+  choices: Choice[];
+  /**
+   *
+   */
+  selectedChoiceId?: string;
+}
+
+/**
+ *
+ */
+export interface Choice {
+  /**
+   *
+   */
+  choiceId: string;
+  /**
+   *
+   */
+  choiceText: string;
+  /**
+   *
+   */
+  choicePayload?: string;
+}
+
+// User message
+
+/**
+ *
+ */
+export interface UserResponse {
+  /**
+   * The type of the response is `"bot"` for bot and `"user"` for user.
+   */
+  type: "user";
+  /**
+   *
+   */
+  receivedAt: Time;
+  /**
+   *
+   */
+  payload: UserResponsePayload;
+}
+
+/**
+ *
+ */
+export type UserResponsePayload =
+  | {
+      /**
+       *
+       */
+      type: "text";
+      /**
+       *
+       */
+      text: string;
+      /**
+       *
+       */
+      context?: Context;
+    }
+  | {
+      /**
+       *
+       */
+      type: "choice";
+      /**
+       *
+       */
+      choiceId: string;
+      /**
+       *
+       */
+      context?: Context;
+    }
+  | ({
+      /**
+       *
+       */
+      type: "structured";
+      /**
+       *
+       */
+      context?: Context;
+    } & StructuredRequest);
+
+// Failure message
+
+/**
+ *
+ */
+export interface FailureMessage {
+  /**
+   *
+   */
+  type: "failure";
+  /**
+   *
+   */
+  payload: {
+    /**
+     *
+     */
+    text: string;
+  };
+  /**
+   *
+   */
+  receivedAt: Time;
+}
+
+/**
+ *
+ */
+export type Response = BotResponse | UserResponse | FailureMessage;
+
+/**
+ *
+ */
+export type Time = number;
+
+// Config and state
+
+/**
+ *
+ */
+export type Environment = "production" | "development";
+
+/**
+ * The config to create a conversation.
+ * `botUrl` and
+ */
+export interface Config {
+  /**
+   * Fetch this from the bot's Deployment page.
+   */
+  botUrl: string;
+  /**
+   *
+   */
+  conversationId?: string;
+  /**
+   *
+   */
+  userId?: string;
+  /**
+   *
+   */
+  responses?: Response[];
+  /**
+   *
+   */
+  failureMessage?: string;
+  /**
+   *
+   */
+  environment?: Environment;
+  /**
+   *
+   */
+  headers: Record<string, string> & { "nlx-api-key": string };
+  /**
+   *
+   */
+  languageCode: string;
+  // Experimental settings
+  /**
+   *
+   */
+  experimental?: {
+    // Simulate alternative channel types
+    /**
+     *
+     */
+    channelType?: string;
+    // Prevent the `languageCode` parameter to be appended to the bot URL - used in special deployment environments such as the sandbox chat inside Dialog Studio
+    /**
+     *
+     */
+    completeBotUrl?: boolean;
+  };
+}
+
+const welcomeIntent = "NLX.Welcome";
+
+const defaultFailureMessage = "We encountered an issue. Please try again soon.";
+
+/**
+ *
+ */
+export type State = Response[];
+
+const normalizeSlots = (
+  slotsRecordOrArray: SlotsRecordOrArray,
+): SlotValue[] => {
+  if (Array.isArray(slotsRecordOrArray)) {
+    return slotsRecordOrArray;
+  }
+  return Object.entries(slotsRecordOrArray).map(([key, value]) => ({
+    slotId: key,
+    value,
+  }));
 };
 
 /**
  *
- * @param fn - the function to wrap
- * @param convo - the whole conversation handler (from {@link createConversation})
- * @param timeout - the timeout in milliseconds, defaults to 10,000
- * @returns A wrapped version of the function that returns a promise with its response.
+ */
+export interface StructuredRequest {
+  /**
+   *
+   */
+  choiceId?: string;
+  /**
+   *
+   */
+  intentId?: string;
+  /**
+   *
+   */
+  nodeId?: string;
+  /**
+   *
+   */
+  slots?: SlotsRecordOrArray;
+  /**
+   *
+   */
+  poll?: boolean;
+}
+
+interface BotRequest {
+  conversationId?: string;
+  userId?: string;
+  context?: Context;
+  request: {
+    unstructured?: {
+      text: string;
+    };
+    structured?: StructuredRequest & { slots?: SlotValue[] };
+  };
+}
+
+/**
+ *
+ */
+export interface ChoiceRequestMetadata {
+  /**
+   *
+   */
+  responseIndex?: number;
+  /**
+   *
+   */
+  messageIndex?: number;
+  /**
+   *
+   */
+  nodeId?: string;
+}
+
+interface InternalState {
+  responses: Response[];
+  conversationId: string;
+  userId?: string;
+}
+
+const fromInternal = (internalState: InternalState): State =>
+  internalState.responses;
+
+const safeJsonParse = (val: string): any => {
+  try {
+    const json = JSON.parse(val);
+    return json;
+  } catch (_err) {
+    return null;
+  }
+};
+
+/**
+ *
+ */
+export type Subscriber = (response: Response[], newResponse?: Response) => void;
+
+/**
+ * Helper method to decide when a new {@link Config} requires creating a new {@link ConversationHandler} or whether the old `Config`'s
+ * `ConversationHandler` can be used.
+ *
+ * The order of configs doesn't matter.
+ * @param config1 -
+ * @param config2 -
+ * @returns true if `createConversation` should be called again
+ */
+export const shouldReinitialize = (
+  config1: Config,
+  config2: Config,
+): boolean => {
+  return !equals(
+    omit(["failureMessage"], config1),
+    omit(["failureMessage"], config2),
+  );
+};
+
+/**
+ * This package is intentionally designed with a subscription-based API as opposed to a promise-based one where each message corresponds to a single bot response, available asynchronously.
+ *
+ * If you need a promise-based wrapper, you can use the `promisify` helper available in the package:
+ * @example
+ * ```typescript
+ * import { createConversation, promisify } from "@nlxai/chat-core";
+ *
+ * const convo = createConversation(config);
+ *
+ * const sendTextWrapped = promisify(convo.sendText, convo);
+ *
+ * sendTextWrapped("Hello").then((response) => {
+ *   console.log(response);
+ * });
+ * ```
+ * @typeParam T - the type of the function's params, e.g. for `sendText` it's `text: string, context?: Context`
+ * @param fn - the function to wrap (e.g. `convo.sendText`, `convo.sendChoice`, etc.)
+ * @param convo - the `ConversationHandler` (from {@link createConversation})
+ * @param timeout - the timeout in milliseconds
+ * @returns A wrapped version of the function that returns a promise that resolves to the Conversation's next response.
  */
 export function promisify<T>(
   fn: (payload: T) => void,
@@ -909,5 +939,3 @@ export function promisify<T>(
     });
   };
 }
-
-export default createConversation;
--- packages/website/src/content/05-02-headless-api-reference.tsx
+++ packages/website/src/content/05-02-headless-api-reference.tsx
@@ -1,55 +1,7 @@
 import React from "react";
 import { PageTitle } from "../components/PageTitle";
 import { PageContent } from "../components/PageContent";
-import { coreSetupSnippet } from "../snippets";
-
-export const content = `
-The main method exported by this package is \`createConversation\`, which takes the bot configuration and returns a conversation handler.
-
-~~~js
-${coreSetupSnippet}
-~~~
-
-## Handler API
-
-The conversation handler has the following methods. For each send method, conversation context can be optionally specified as a second argument.
-
-### \`sendText: (text: string, context?: Record<string, unknown>) => void\`
-
-Send a simple text to your bot.
-
-### \`sendChoice: (choiceId: string, context?: Record<string, unknown>) => void\`
-
-Your bot may send a list of choices to choose from, each with a \`choiceText\` and a \`choiceId\` field. You can use \`choiceText\` as button labels, and include the \`choiceId\` in this method when sending responses.
-
-### \`sendSlots: (slots: Record<string, unknown>, context?: Record<string, unknown>) => void\`
-
-Send slot values directly through custom widgets such as interactive maps.
-
-### \`sendIntent: (intentId: string, context?: Record<string, unknown>) => void\`
-
-Trigger a specific intent. The most common use of this method is to show welcome messages by sending the \`NLX.Welcome\` intent.
-
-### \`sendStructured: (request: StructuredRequest, context?: Record<string, unknown>) => void\`
-
-Send a combination of choice, slots, and intent ID in one request.
-
-### \`subscribe: (subscriber: (responses: Response[], newResponse: Response | undefined) => void) => void\`
-
-Subscribe to the current state of messages whenever there is a change. The second argument returns the new response that is triggering the subscription, if there is one.
-
-### \`unsubscribe: (subscriber: (responses: Response[]) => void) => void\`
-
-Remove a subscription.
-
-### \`unsubscribeAll: () => void\`
-
-Remove all subscriptions.
-
-### \`reset: () => void\`
-
-Reset the conversation. This makes sure that information previously collected by your bot will not affect the logic of the conversation any longer.
-`;
+import content from "./06-03-multimodal-api-reference.md?raw";
 
 // initial eslint integration
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
