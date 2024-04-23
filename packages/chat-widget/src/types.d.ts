087449f01d7a498b2aed299f74fe43cc6ad15ca4
Michael Glass <me@mike.is>
2024-04-23 19:50:50 +0200

    cleanup private type overrides

--- packages/chat-widget/src/types.d.ts
+++ packages/chat-widget/src/types.d.ts
@@ -1,25 +1,9 @@
 import "@emotion/react";
+import type { Theme as WidgetTheme } from "./theme";
 
 declare module "@emotion/react" {
-  // Duplicate of theme declaration at src/theme.ts, plus extra fields
-  export interface Theme {
-    /** Primary color for interactive UI elements like buttons */
-    primaryColor: string;
-    /** Background color for the dark chat bubbles (sent by the user) */
-    darkMessageColor: string;
-    /** Background color for the light chat bubbles (sent by the bot) */
-    lightMessageColor: string;
-    /** Customized shade of white */
-    white: string;
-    /** Customized shade of off-white, used in the widget top and bottom bar */
-    fontFamily: string;
-    /** Main spacing unit */
-    spacing: number;
-    /** Chat border radius */
-    borderRadius: number;
-    /** Max height of the chat window */
-    chatWindowMaxHeight: number;
-    // Extra fields
+  // Private theme properties
+  interface Theme extends WidgetTheme {
     windowInnerHeight: number | null;
   }
 }
