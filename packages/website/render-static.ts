4cec18dbfc9ae50f4cf9e34f8aca12a5e24533dc
Michael Glass <me@mike.is>
2024-04-30 06:44:36 -0400

    resolve eslint

--- packages/website/render-static.ts
+++ packages/website/render-static.ts
@@ -1,11 +1,10 @@
 import fs from "node:fs/promises";
 import { exit } from "node:process";
-// @ts-ignore
-import { render as untypedRender } from "./dist/static/entry-server.js";
-const render: (url: string) => html = untypedRender;
-
 import { createServer } from "vite";
 import { dirname } from "node:path";
+// @ts-expect-error we need to build this before usage to ensure cache-busted assets are included, but building right now doesn't build .d.ts files
+import { render as untypedRender } from "./dist/static/entry-server.js";
+const render: (url: string) => string = untypedRender;
 
 const server = await createServer({
   server: { middlewareMode: true },
@@ -27,13 +26,13 @@ await Promise.all(
 
 await renderTo("/", "./dist/client/index.html");
 
-async function renderTo(url: string, destination: string) {
-  const rendered = await render(url);
+async function renderTo(url: string, destination: string): Promise<void> {
+  const rendered = render(url);
 
   const html = template.replace(`<!--app-html-->`, rendered ?? "");
 
   await fs.mkdir(dirname(destination), { recursive: true });
-  return fs.writeFile(destination, html);
+  await fs.writeFile(destination, html);
 }
 
 exit(0);
