297cbe2ec366c7178288e2295450658dcf3b1d43
Michael Glass <me@mike.is>
2024-05-02 10:26:53 -0400

    whitespace

--- packages/website/src/custom-components/Disclaimer.tsx
+++ packages/website/src/custom-components/Disclaimer.tsx
@@ -8,45 +8,45 @@ const html = htm.bind(createElement);
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const Disclaimer = () => {
   const [status, setStatus] = useState<"pending" | "accepted" | "denied">(
-    "pending"
+    "pending",
   );
   return html`
     <div className="chat-disclaimer">
       ${status === "pending" &&
-        html`
-          <p>
-            In order to enhance your experience in this chat, we would like to
-            temporarily store personal data according to our${" "}
-            <a>privacy policy</a>.
-          </p>
-          <button
-            onClick=${() => {
-              setStatus("accepted");
-            }}
-          >
-            Accept
-          </button>
-          <button
-            onClick=${() => {
-              setStatus("denied");
-            }}
-          >
-            Deny
-          </button>
-        `}
+      html`
+        <p>
+          In order to enhance your experience in this chat, we would like to
+          temporarily store personal data according to our${" "}
+          <a>privacy policy</a>.
+        </p>
+        <button
+          onClick=${() => {
+            setStatus("accepted");
+          }}
+        >
+          Accept
+        </button>
+        <button
+          onClick=${() => {
+            setStatus("denied");
+          }}
+        >
+          Deny
+        </button>
+      `}
       ${status === "accepted" &&
-        html`
-          <p>
-            As requested, we might store certain personal or device identifiers
-            to enhance your experience on this chat.
-          </p>
-        `}
+      html`
+        <p>
+          As requested, we might store certain personal or device identifiers to
+          enhance your experience on this chat.
+        </p>
+      `}
       ${status === "denied" &&
-        html`
-          <p>
-            As requested, we will not store personal information in this chat.
-          </p>
-        `}
+      html`
+        <p>
+          As requested, we will not store personal information in this chat.
+        </p>
+      `}
     </div>
   `;
 };
