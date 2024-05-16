8fe4a9e590d21c2133efdb287f984ba21599169a
Peter Szerzo <szerzo.peter@gmail.com>
2024-05-16 16:16:42 +0200

    Expand on snippets

--- packages/website/src/content/02-03-web-widget-custom-behaviors.tsx
+++ packages/website/src/content/02-03-web-widget-custom-behaviors.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, type FC } from "react";
 import { PageTitle } from "../components/PageTitle";
 import { PageContent } from "../components/PageContent";
 import { BehaviorEditor } from "../components/ChatConfiguration";
@@ -8,9 +8,7 @@ export const content = `
 The widget can be configured to handle a number of custom behaviors. Select one from below and see how the code snippet changes:
 `;
 
-// initial eslint integration
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const codeContent = (behavior: Behavior) => `
+export const codeContent = (behavior: Behavior): string => `
 ~~~html
 ${setupSnippet({
   config: { botUrl: "", languageCode: "en-US", headers: { "nlx-api-key": "" } },
@@ -20,9 +18,7 @@ ${setupSnippet({
 ~~~
 `;
 
-// initial eslint integration
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const WebWidgetCustomBehaviors = () => {
+export const WebWidgetCustomBehaviors: FC<unknown> = () => {
   const [behavior, setBehavior] = useState<Behavior>(Behavior.Simple);
   return (
     <>
