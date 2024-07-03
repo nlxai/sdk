45b94d38f4ae4aa824bf70cc73bf0fd98ed29c12
Michael Glass <me@mike.is>
2024-07-03 12:22:08 +0200

    extract rollup config into its own package

--- packages/chat-core/rollup.config.mjs
+++ packages/chat-core/rollup.config.mjs
@@ -1,39 +1,9 @@
-// This file is mostly identical to that of other bundled packages, except for the bundled input path and output name
-import commonjs from "@rollup/plugin-commonjs";
-import json from "@rollup/plugin-json";
-import nodePolyfills from "rollup-plugin-node-polyfills";
-import nodeResolve from "@rollup/plugin-node-resolve";
-import terser from "@rollup/plugin-terser";
-import typescript from "@rollup/plugin-typescript";
+import rollupConfig from "rollup-config-nlx";
 import pkg from "./package.json" assert { type: "json" };
 
-export default [
-  // Browser-friendly UMD build. This version is used with unpkg.com to be directly included in a script tag.
-  {
-    input: "src/index.ts",
-    output: {
-      name: "nlxai.chatCore",
-      file: pkg.browser,
-      format: "umd",
-    },
-    plugins: [
-      commonjs(), // needed to bundle node modules that use cjs
-      json(), // needed to fetch package version from package.json
-      nodePolyfills(),
-      nodeResolve({ browser: true }), // bundles packages from node_modules
-      terser(),
-      typescript(),
-    ],
-  },
-  // Bundler/Node-friendly CommonJS and ESM builds
-  // this version doesn't need to package node_modules because we assume they'll be installed as dependencies of this package
-  {
-    input: "src/index.ts",
-    output: [
-      { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "es" },
-    ],
-    external: ["isomorphic-fetch", "reconnecting-websocket", "uuid", "ramda"],
-    plugins: [typescript(), json()],
-  },
-];
+export default rollupConfig(
+  pkg,
+  "nlxai.chatCore",
+  ["isomorphic-fetch", "reconnecting-websocket", "uuid", "ramda"],
+  true,
+);
---
+++ packages/rollup-config-nlx/index.js
@@ -0,0 +1,44 @@
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import nodePolyfills from "rollup-plugin-node-polyfills";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+
+export default function buildconfig(pkg, name, externalDeps, buildBrowser) {
+  const config = [
+    // Bundler/Node-friendly CommonJS and ESM builds
+    // this version doesn't need to package node_modules because we assume they'll be installed as dependencies of this package
+    {
+      input: "src/index.ts",
+      output: [
+        { file: pkg.main, format: "cjs" },
+        { file: pkg.module, format: "es" },
+      ],
+      external: externalDeps,
+      plugins: [typescript(), json()],
+    },
+  ];
+
+  if (buildBrowser) {
+    // Browser-friendly UMD build. This version is used with unpkg.com to be directly included in a script tag.
+    config.push({
+      input: "src/index.ts",
+      output: {
+        name: name,
+        file: pkg.browser,
+        format: "umd",
+      },
+      plugins: [
+        commonjs(), // needed to bundle node modules that use cjs
+        json(), // needed to fetch package version from package.json
+        nodePolyfills(),
+        nodeResolve({ browser: true }), // bundles packages from node_modules
+        terser(),
+        typescript(),
+      ],
+    });
+  }
+
+  return config;
+}
