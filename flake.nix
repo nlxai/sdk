bd37b72bb04d102e9ef73af7ad828cce5380ce07
Michael Glass <me@mike.is>
2024-08-29 13:16:03 +0200

    downgrade node to 20
    see https://github.com/rollup/plugins/issues/1762

--- flake.nix
+++ flake.nix
@@ -5,38 +5,34 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-    let
-      assertVersion = version: pkg: (
-        assert (pkgs.lib.assertMsg (builtins.toString pkg.version == version) ''
-          Expecting version of ${pkg.name} to be ${version} but got ${pkg.version};
-        '');
-        pkg
-      );
-      pkgs = import nixpkgs
-        {
+      let
+        assertVersion = version: pkg:
+          (assert (pkgs.lib.assertMsg
+            (builtins.toString pkg.version == version) ''
+              Expecting version of ${pkg.name} to be ${version} but got ${pkg.version};
+            '');
+            pkg);
+        pkgs = import nixpkgs {
           system = system;
           config.allowUnfree = true;
         };
 
-    in
-    {
-      formatter = pkgs.nixpkgs-fmt;
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
-          (assertVersion (nixpkgs.lib.strings.fileContents "${self}/.node-version") nodejs_22)
-          # caddy is used to mimic github actions more closely than the vite dev server. Not used in production
-          (assertVersion "2.7.6" caddy)
-          # comrak is used to preview rendered markdown. Not used in production.
-          (assertVersion "0.23.0" comrak)
-          # used for testing dead website links
-          (assertVersion "1.21.4" wget)
-        ];
-      };
-    });
+      in {
+        formatter = pkgs.nixpkgs-fmt;
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            (assertVersion
+              (nixpkgs.lib.strings.fileContents "${self}/.node-version")
+              nodejs_20)
+            # caddy is used to mimic github actions more closely than the vite dev server. Not used in production
+            (assertVersion "2.7.6" caddy)
+            # comrak is used to preview rendered markdown. Not used in production.
+            (assertVersion "0.23.0" comrak)
+            # used for testing dead website links
+            (assertVersion "1.21.4" wget)
+          ];
+        };
+      });
 }
