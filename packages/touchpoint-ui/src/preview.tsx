3042a3944be0bb3f965abe400f9ef51aed9d20a8
Peter Szerzo <szerzo.peter@gmail.com>
2025-02-27 15:40:48 +0100

    Remove context

--- packages/touchpoint-ui/src/preview.tsx
+++ packages/touchpoint-ui/src/preview.tsx
@@ -3,7 +3,6 @@
 import { type FC, type ReactNode } from "react";
 import type { ColorMode, Theme } from "./types";
 import { CustomPropertiesContainer } from "./components/Theme";
-import { Context } from "./context";
 import cssRaw from "./index.css?inline";
 /**
  * @internal
@@ -16,15 +15,13 @@ export const Container: FC<{
   return (
     <>
       <style>{cssRaw}</style>
-      <Context.Provider value={{ handler: null }}>
-        <CustomPropertiesContainer
-          className="bg-background p-4 rounded-outer space-y-4"
-          theme={theme}
-          colorMode={mode}
-        >
-          {children}
-        </CustomPropertiesContainer>
-      </Context.Provider>
+      <CustomPropertiesContainer
+        className="bg-background p-4 rounded-outer space-y-4"
+        theme={theme}
+        colorMode={mode}
+      >
+        {children}
+      </CustomPropertiesContainer>
     </>
   );
 };
