a59f9d711c92971c199d7598d8a35c3640f5b84c
Peter Szerzo <szerzo.peter@gmail.com>
2025-04-09 10:25:55 +0200

    Fix linter

--- packages/touchpoint-ui/src/globals.d.ts
+++ packages/touchpoint-ui/src/globals.d.ts
@@ -1,4 +1,6 @@
 declare global {
+  // Needs a type parameter because of the way it's used in `livekit-client`
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   type FinalizationRegistry<T> = any;
 }
 
