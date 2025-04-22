5ecce8a658d64166f93f5cb699a2fb73fc37300e
Jakub Hampl <jakub@nlx.ai>
2025-04-22 12:59:54 +0300

    Fixes build on node >=22

--- packages/voice-plus-core/rollup.config.ts
+++ packages/voice-plus-core/rollup.config.ts
@@ -1,5 +1,5 @@
 import rollupConfig from "rollup-config-nlx";
-import pkg from "./package.json" assert { type: "json" };
+import pkg from "./package.json" with { type: "json" };
 
 export default rollupConfig({
   pkg: pkg,
