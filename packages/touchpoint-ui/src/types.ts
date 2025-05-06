ca768454ed5a6678f37e19df728fc8634c4bc310
Peter Szerzo <szerzo.peter@gmail.com>
2025-05-06 17:56:20 +0200

    Experimental Voice+ rendering for Touchpoint

--- packages/touchpoint-ui/src/types.ts
+++ packages/touchpoint-ui/src/types.ts
@@ -222,5 +222,5 @@ export interface TouchpointConfiguration {
   /**
    * Controls the ways in which the user can communicate with the application. Defaults to `"text"`
    */
-  input?: "text" | "voice";
+  input?: "text" | "voice" | "experimentalVoicePlus";
 }
