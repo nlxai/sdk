44a0f1a17721e87ba57a1772823499499049596a
Jakub Hampl <161446658+jakub-nlx@users.noreply.github.com>
2025-05-23 14:59:07 +0100

    Handle disconnect better (#246)

--- packages/touchpoint-ui/src/voice.ts
+++ packages/touchpoint-ui/src/voice.ts
@@ -11,7 +11,12 @@ import {
 import { useCallback, useEffect, useRef, useState } from "react";
 import { useDebouncedState } from "@react-hookz/web";
 
-type VoiceRoomState = "inactive" | "pending" | "active" | "error";
+type VoiceRoomState =
+  | "inactive"
+  | "pending"
+  | "active"
+  | "error"
+  | "terminated";
 
 export interface SoundCheck {
   micAllowed: boolean;
@@ -110,10 +115,14 @@ export const useVoice = ({
     useDebouncedState<boolean>(false, 600);
 
   const disconnect = useCallback(() => {
-    roomRef.current?.disconnect().catch((err) => {
+    if (roomRef.current == null) {
+      return;
+    }
+    roomRef.current.disconnect().catch((err) => {
       // eslint-disable-next-line no-console
       console.warn(err);
     });
+    void handler.terminateLiveKitCall();
     roomRef.current = null;
     // Not 100% sure this is necessary but it seems to help
     if (audioElementRef.current != null) {
@@ -173,6 +182,12 @@ export const useVoice = ({
         ParticipantEvent.IsSpeakingChanged,
         handleIsSpeakingChanged,
       );
+      room.on(RoomEvent.Disconnected, () => {
+        setRoomState("terminated");
+        setIsUserSpeaking(false);
+        setIsApplicationSpeaking(false);
+        disconnect();
+      });
 
       await room.connect(creds.url, creds.token, { autoSubscribe: true });
       await room.localParticipant.setMicrophoneEnabled(true);
@@ -190,13 +205,10 @@ export const useVoice = ({
   }, [setRoomState, handler, setIsUserSpeaking, setIsApplicationSpeaking]);
 
   useEffect(() => {
-    if (!active) {
-      disconnect();
-      return;
+    if (active) {
+      void setupRoom();
     }
-    // This function call will never throw, therefore the floating promises rule should not apply
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    setupRoom();
+    return disconnect;
   }, [active, disconnect, setupRoom]);
 
   return {
