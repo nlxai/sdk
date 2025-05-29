4dfcf3eb0c46bd6f9255b6f06c7b2733e3156460
Peter Szerzo <szerzo.peter@gmail.com>
2025-05-29 15:14:16 +0200

    Fix eslint

--- packages/touchpoint-ui/src/voice.ts
+++ packages/touchpoint-ui/src/voice.ts
@@ -65,6 +65,7 @@ export const useVoice = ({
       return;
     }
     room.localParticipant.setMicrophoneEnabled(micEnabled).catch((err) => {
+      // eslint-disable-next-line no-console
       console.warn(err);
     });
   }, [micEnabled]);
