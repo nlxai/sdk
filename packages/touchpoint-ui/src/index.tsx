39c823ffffe87c840808de8866faced4bd80c0a8
Peter Szerzo <szerzo.peter@gmail.com>
2025-06-03 16:48:32 +0200

    Use non-deprecated welcome flow message

--- packages/touchpoint-ui/src/index.tsx
+++ packages/touchpoint-ui/src/index.tsx
@@ -144,7 +144,7 @@ class NlxTouchpointElement extends HTMLElement {
               this.#touchpointConfiguration.initializeConversation ??
               ((handler, context) => {
                 if ((this.#touchpointConfiguration?.input ?? "text") === "text")
-                  handler.sendWelcomeIntent(context);
+                  handler.sendWelcomeFlow(context);
               })
             }
             onClose={this.onClose}
