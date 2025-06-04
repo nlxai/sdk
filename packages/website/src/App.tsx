0580b6b07478c478f01b418a638118eecbe88b0f
Peter Szerzo <szerzo.peter@gmail.com>
2025-06-04 19:03:41 +0200

    Custom language for touchpoint-ui code (#256)
    * Custom language for touchpoint-ui code
    
    * Expand imports list
    
    * Fix typecheck
    
    * touchpointui-lang adjustments
    
    * Fix copy-paste
    
    * Document touchpoint UI cdn version
    
    * Use dynamic package version
    
    * Set up context for editing environment for entire page

--- packages/website/src/App.tsx
+++ packages/website/src/App.tsx
@@ -3,29 +3,34 @@ import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
 import { Nav, MobileNav } from "./components/Nav";
 import { ContentRoutes } from "./routes";
+import { ReactContext } from "./context";
+import { type SnippetEnv } from "./types";
 
 export const App: FC<unknown> = () => {
   const [mobileMenuExpanded, setMobileMenuExpanded] = useState<boolean>(false);
+  const [snippetEnv, setSnippetEnv] = useState<SnippetEnv>("html");
 
   return (
-    <div className="flex w-full flex-col text-primary-80">
-      <Header
-        mobileMenuExpanded={mobileMenuExpanded}
-        setMobileMenuExpanded={setMobileMenuExpanded}
-      />
-      {mobileMenuExpanded && (
-        <MobileNav setMobileMenuExpanded={setMobileMenuExpanded} />
-      )}
-      <Hero />
-      <div className="relative mx-auto flex w-full max-w-8xl flex-auto justify-center">
-        <Nav />
-        <div className="min-w-0 max-w-2xl flex-auto px-4 lg:px-8 py-16 lg:max-w-none xl:px-16">
-          <article>
-            <ContentRoutes />
-          </article>
+    <ReactContext.Provider value={{ snippetEnv, setSnippetEnv }}>
+      <div className="flex w-full flex-col text-primary-80">
+        <Header
+          mobileMenuExpanded={mobileMenuExpanded}
+          setMobileMenuExpanded={setMobileMenuExpanded}
+        />
+        {mobileMenuExpanded && (
+          <MobileNav setMobileMenuExpanded={setMobileMenuExpanded} />
+        )}
+        <Hero />
+        <div className="relative mx-auto flex w-full max-w-8xl flex-auto justify-center">
+          <Nav />
+          <div className="min-w-0 max-w-2xl flex-auto px-4 lg:px-8 py-16 lg:max-w-none xl:px-16">
+            <article>
+              <ContentRoutes />
+            </article>
+          </div>
         </div>
       </div>
-    </div>
+    </ReactContext.Provider>
   );
 };
 
