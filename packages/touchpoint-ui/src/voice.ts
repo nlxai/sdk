e243b42423da7c60eb2e7ea2c6fbbc702fc190cb
Peter Szerzo <szerzo.peter@gmail.com>
2025-06-23 11:54:03 +0200

    Remove terminate calls

--- packages/touchpoint-ui/src/voice.ts
+++ packages/touchpoint-ui/src/voice.ts
@@ -177,7 +177,6 @@ export const useVoice = ({
       // eslint-disable-next-line no-console
       console.warn(err);
     });
-    void handler.terminateVoiceCall();
     if (trackRef.current != null) {
       trackRef.current.stop();
       trackRef.current = null;
@@ -263,10 +262,6 @@ export const useVoice = ({
       setRoomState("error");
       // eslint-disable-next-line no-console
       console.warn(err);
-      handler.terminateVoiceCall().catch((err: any) => {
-        // eslint-disable-next-line no-console
-        console.warn(err);
-      });
     }
   }, [
     setRoomState,
