ef6134c4203f2ea71eb79134b078bb7aee4097d0
Peter Szerzo <szerzo.peter@gmail.com>
2025-07-21 11:01:52 +0200

    Remove unused import

--- packages/touchpoint-ui/src/components/VoiceMini.tsx
+++ packages/touchpoint-ui/src/components/VoiceMini.tsx
@@ -8,15 +8,7 @@ import { useVoice } from "../voice";
 import { LoaderAnimation } from "./ui/Loader";
 import { Ripple } from "./Ripple";
 import { IconButton } from "./ui/IconButton";
-import {
-  Close,
-  Mic,
-  MicOff,
-  Volume,
-  VolumeOff,
-  Restart,
-  Undo,
-} from "./ui/Icons";
+import { Close, Mic, MicOff, Volume, VolumeOff, Restart } from "./ui/Icons";
 import { TextButton } from "./ui/TextButton";
 import { VoiceModalities } from "./FullscreenVoice";
 import { ErrorMessage } from "./ErrorMessage";
