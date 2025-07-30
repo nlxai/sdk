37c4b4324e091df6b4025e72b00b250ef27183e9
Peter Szerzo <szerzo.peter@gmail.com>
2025-07-30 09:46:18 +0200

    Factor out default modality save logic

--- packages/touchpoint-ui/src/components/defaultModalities/DefaultDateInput.tsx
+++ packages/touchpoint-ui/src/components/defaultModalities/DefaultDateInput.tsx
@@ -1,23 +1,32 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable jsdoc/require-jsdoc */
+import { useMemo } from "react";
+import { type ConversationHandler } from "@nlxai/core";
+
 import { type SaveAs } from "./types";
 import { type CustomModalityComponent } from "../../types";
 import { DateInput } from "../ui/DateInput";
 
+const saveFn = (
+  $saveAs: SaveAs,
+  conversationHandler: ConversationHandler,
+): ((val: any) => void) => {
+  return (val) => {
+    if ($saveAs.type === "slot") {
+      conversationHandler.sendSlots({ [$saveAs.id]: val });
+    }
+    if ($saveAs.type === "context") {
+      conversationHandler.sendContext({ [$saveAs.id]: val });
+    }
+  };
+};
+
 export const DefaultDateInput: CustomModalityComponent<{
   $saveAs: SaveAs;
 }> = ({ data, conversationHandler }) => {
-  return (
-    <DateInput
-      onSubmit={(date) => {
-        const { $saveAs } = data;
-        if ($saveAs.type === "slot") {
-          conversationHandler.sendSlots({ [$saveAs.id]: date });
-        }
-        if ($saveAs.type === "context") {
-          conversationHandler.sendContext({ [$saveAs.id]: date });
-        }
-      }}
-    />
+  const save = useMemo(
+    () => saveFn(data.$saveAs, conversationHandler),
+    [data.$saveAs, conversationHandler],
   );
+  return <DateInput onSubmit={save} />;
 };
