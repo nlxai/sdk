0e69818724da119974b4d48e64804f10a2675275
Peter Szerzo <szerzo.peter@gmail.com>
2025-09-30 14:06:27 +0200

    Log obvious data mistakes on default modalities

--- packages/touchpoint-ui/src/components/defaultModalities/DefaultCarousel.tsx
+++ packages/touchpoint-ui/src/components/defaultModalities/DefaultCarousel.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable jsdoc/require-jsdoc */
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import { type CustomModalityComponent } from "../../interface";
 import { Carousel } from "../ui/Carousel";
 import {
@@ -10,21 +10,33 @@ import {
 } from "../ui/CustomCard";
 import { BaseText } from "../ui/Typography";
 import { type SaveAs, type CardData, saveFn } from "./shared";
+import { debug } from "../debug";
 
 export const DefaultCarousel: CustomModalityComponent<{
   cards: CardData[];
-  $saveAs: SaveAs;
+  $saveAs?: SaveAs;
 }> = ({ data, conversationHandler }) => {
   const [selectedCard, setSelectedCard] = useState<string | null>(null);
 
+  useEffect(() => {
+    if (data.cards == null) {
+      debug(
+        "Expecting the 'DefaultCarousel' custom modality to be triggered with an array field called 'cards', but the field is missing.",
+      );
+    }
+  }, [data]);
+
   const save = useMemo(
-    () => saveFn(data.$saveAs, conversationHandler),
+    () =>
+      data.$saveAs == null
+        ? undefined
+        : saveFn(data.$saveAs, conversationHandler),
     [data.$saveAs, conversationHandler],
   );
 
   const handleClick = (cardId: string): void => {
     setSelectedCard(cardId);
-    save(cardId);
+    save?.(cardId);
   };
 
   return (
