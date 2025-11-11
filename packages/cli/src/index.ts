ff1c64a581b8aac9e05dbe3a4890ec383b7e79f8
Jakub Hampl <jakub@nlx.ai>
2025-11-11 15:37:02 +0000

    Adds a test runner

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -3,6 +3,7 @@ import { authCommand } from "./commands/auth/index.js";
 import { modalitiesCommand } from "./commands/modalities/index.js";
 import { dataRequestsCommand } from "./commands/data-requests/index.js";
 import { httpCommand } from "./commands/http.js";
+import { testCommand } from "./commands/test.js";
 
 program.description(
   "Keep your Typescript types and NLX schema definitions in sync",
@@ -11,5 +12,6 @@ program.addCommand(authCommand);
 program.addCommand(modalitiesCommand);
 program.addCommand(dataRequestsCommand);
 program.addCommand(httpCommand);
+program.addCommand(testCommand);
 
 program.parse(process.argv);
