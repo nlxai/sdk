8cc2eeba78dc8f449fea767125dc9ee71f89051e
Jakub Hampl <jakub@nlx.ai>
2025-11-12 13:31:28 +0000

    Add verbose mode for easier debugging

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -4,8 +4,17 @@ import { modalitiesCommand } from "./commands/modalities/index.js";
 import { dataRequestsCommand } from "./commands/data-requests/index.js";
 import { httpCommand } from "./commands/http.js";
 import { testCommand } from "./commands/test.js";
+import { consola } from "consola";
 
 program.description("Intereact with NLX from the command line");
+program
+  .option("-v, --verbose", "Enable verbose logging")
+  .hook("preAction", (thisCommand) => {
+    if (thisCommand.opts().verbose) {
+      consola.level = 4; // Debug level
+    }
+  });
+program.configureHelp({ showGlobalOptions: true });
 program.addCommand(authCommand);
 program.addCommand(modalitiesCommand);
 program.addCommand(dataRequestsCommand);
