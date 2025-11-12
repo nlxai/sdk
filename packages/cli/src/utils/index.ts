825c4bb144a14723d5aa35ef12cf12b8d96016ea
Jakub Hampl <jakub@nlx.ai>
2025-11-12 13:14:45 +0000

    Adds skipping tests not marked for build

--- packages/cli/src/utils/index.ts
+++ packages/cli/src/utils/index.ts
@@ -4,12 +4,13 @@ import { consola } from "consola";
 const API_BASE_URL =
   process.env.NLX_API_BASE_URL || "https://api.dev.studio.nlx.ai/v1";
 
-export const fetchManagementApi = async (
+export const fetchManagementApi = async <T extends unknown>(
   path: string,
   method: string = "GET",
   body?: any,
-) => {
+): Promise<T> => {
   const accessToken = await ensureToken();
+  consola.debug(`Fetch ${method} /${path} ${body ? JSON.stringify(body) : ""}`);
   const res = await fetch(`${API_BASE_URL}/${path}`, {
     headers: {
       Authorization: `Bearer ${accessToken}`,
@@ -22,5 +23,7 @@ export const fetchManagementApi = async (
     consola.error("Failed to fetch:", res.status, await res.text());
     process.exit(1);
   }
-  return await res.json();
+  const result = (await res.json()) as T;
+  consola.debug("Response:", JSON.stringify(result));
+  return result;
 };
