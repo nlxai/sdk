8330ce508b9b378c63c0cd775c0efd8ba5d8389b
Peter Szerzo <szerzo.peter@gmail.com>
2026-01-21 11:24:42 +0100

    Fix alignment

--- packages/touchpoint-ui/src/components/Notice.tsx
+++ packages/touchpoint-ui/src/components/Notice.tsx
@@ -7,7 +7,7 @@ export const Notice: FC<{
   return (
     <div className="grid grid-cols-[1fr_auto_1fr] items-center gap-3">
       <div className="border-b border-solid border-primary-60 h-[1px]"></div>
-      <p className="text-primary-60 w-fit max-w-[220px]">{text}</p>
+      <p className="text-primary-60 text-center w-fit max-w-[220px]">{text}</p>
       <div className="border-b border-solid border-primary-60 h-[1px]"></div>
     </div>
   );
