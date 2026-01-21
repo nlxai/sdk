b6b1c5cdcc635cbf0866d2776fb699101ae2884d
Peter Szerzo <szerzo.peter@gmail.com>
2026-01-21 11:23:15 +0100

    Implement via grid layout

--- packages/touchpoint-ui/src/components/Notice.tsx
+++ packages/touchpoint-ui/src/components/Notice.tsx
@@ -1,46 +1,14 @@
 /* eslint-disable jsdoc/require-jsdoc */
-import { type FC, useRef, useState, useMemo } from "react";
-import { useResizeObserver } from "@react-hookz/web";
+import { type FC } from "react";
 
 export const Notice: FC<{
   text: string;
 }> = ({ text }) => {
-  const containerRef = useRef<HTMLDivElement | null>(null);
-  const textRef = useRef<HTMLParagraphElement | null>(null);
-  const [containerSize, setContainerSize] = useState<
-    ResizeObserverEntry | undefined
-  >();
-  const [textSize, setTextSize] = useState<ResizeObserverEntry | undefined>();
-  useResizeObserver(containerRef, setContainerSize);
-  useResizeObserver(textRef, setTextSize);
-  const borderWidth = useMemo(() => {
-    if (containerSize?.contentRect == null || textSize?.contentRect == null) {
-      return null;
-    }
-    return (
-      (containerSize.contentRect.width - textSize.contentRect.width) / 2 - 12
-    );
-  }, [containerSize, textSize]);
   return (
-    <div
-      ref={containerRef}
-      className="flex items-center justify-center relative"
-    >
-      {borderWidth != null ? (
-        <>
-          <div
-            className="absolute border-b border-solid border-primary-60 h-[1px] left-0 top-1/2 transform -translate-y-1/2"
-            style={{ width: borderWidth + "px" }}
-          ></div>
-          <div
-            className="absolute border-b border-solid border-primary-60 h-[1px] right-0 top-1/2 transform -translate-y-1/2"
-            style={{ width: borderWidth + "px" }}
-          ></div>
-        </>
-      ) : null}
-      <p ref={textRef} className="w-fit text-primary-60 max-w-[220px]">
-        {text}
-      </p>
+    <div className="grid grid-cols-[1fr_auto_1fr] items-center gap-3">
+      <div className="border-b border-solid border-primary-60 h-[1px]"></div>
+      <p className="text-primary-60 w-fit max-w-[220px]">{text}</p>
+      <div className="border-b border-solid border-primary-60 h-[1px]"></div>
     </div>
   );
 };
