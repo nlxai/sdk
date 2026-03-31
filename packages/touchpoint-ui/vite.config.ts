6e700f73f3f7ed438e24ba755a1ae141521dbfa1
Peter Szerzo <szerzo.peter@gmail.com>
2026-03-31 18:26:56 +0200

    Add design system build file

--- packages/touchpoint-ui/vite.config.ts
+++ packages/touchpoint-ui/vite.config.ts
@@ -1,5 +1,6 @@
 import { resolve } from "path";
 import tailwindcss from "@tailwindcss/vite";
+import { type PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 import replace from "@rollup/plugin-replace";
 import { defineConfig } from "vite";
@@ -8,15 +9,14 @@ import dts from "vite-plugin-dts";
 /**
  * Adjust the JS entry point if the app is run in prototype or review modes
  */
-const adjustJsEntryPoint = ({ mode }: { mode: string }): any => {
+const designSystemEntryPoint = (): PluginOption => {
   return {
     name: "html-transform",
     transformIndexHtml: {
       order: "pre",
-      handler: (html: string) => {
-        if (mode === "design-system") {
-          return {
-            html: `<!doctype html>
+      handler: () => {
+        return {
+          html: `<!doctype html>
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
@@ -33,38 +33,47 @@ const adjustJsEntryPoint = ({ mode }: { mode: string }): any => {
 </body>
 </html>
 `,
-            tags: [],
-          };
-        }
-        return html;
+          tags: [],
+        };
       },
     },
   };
 };
 
+const sharedPlugins = (command: "serve" | "build"): PluginOption[] => [
+  react(),
+  tailwindcss(),
+  replace(
+    command === "serve"
+      ? {}
+      : {
+          "process.env.NODE_ENV": JSON.stringify("production"),
+        },
+  ),
+  dts(),
+];
+
 // https://vitejs.dev/config/
-export default defineConfig(({ mode, command }) => ({
-  plugins: [
-    adjustJsEntryPoint({ mode }),
-    react(),
-    tailwindcss(),
-    replace(
-      command === "serve"
-        ? {}
-        : {
-            "process.env.NODE_ENV": JSON.stringify("production"),
+export default defineConfig(({ mode, command }) =>
+  mode === "design-system"
+    ? {
+        plugins: [designSystemEntryPoint(), ...sharedPlugins(command)],
+        build: {
+          outDir: "build",
+        },
+      }
+    : {
+        plugins: sharedPlugins(command),
+        resolve: {},
+        build: {
+          outDir: "lib",
+          lib: {
+            entry: resolve(__dirname, "./src/index.tsx"),
+            type: ["umd", "es"],
+            name: "nlxai.touchpointUi",
+            fileName: (format) =>
+              format === "umd" ? "index.umd.js" : "index.js",
           },
-    ),
-    dts(),
-  ],
-  resolve: {},
-  build: {
-    outDir: "lib",
-    lib: {
-      entry: resolve(__dirname, "./src/index.tsx"),
-      type: ["umd", "es"],
-      name: "nlxai.touchpointUi",
-      fileName: (format) => (format === "umd" ? "index.umd.js" : "index.js"),
-    },
-  },
-}));
+        },
+      },
+);
