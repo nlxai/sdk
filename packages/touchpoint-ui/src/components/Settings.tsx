8665614fc1b8cbab3de40908c5f95fef898e8a95
Peter Szerzo <szerzo.peter@gmail.com>
2026-04-06 10:39:00 +0200

    Add settings page back button

--- packages/touchpoint-ui/src/components/Settings.tsx
+++ packages/touchpoint-ui/src/components/Settings.tsx
@@ -2,10 +2,11 @@
 import { type FC } from "react";
 import { type ConversationHandler } from "@nlxai/core";
 
-import { Escalate, Restart } from "./ui/Icons";
+import { Escalate, Restart, ArrowLeft } from "./ui/Icons";
 import { useCopy } from "../utils/useCopy";
 import { TextButton } from "./ui/TextButton";
 import { clsx } from "clsx";
+import { IconButton } from "./ui/IconButton";
 
 interface SettingsProps {
   onClose: () => void;
@@ -24,10 +25,17 @@ export const Settings: FC<SettingsProps> = ({
   return (
     <div
       className={clsx(
-        "p-2 md:p-3 flex flex-col grow justify-center gap-2",
+        "p-2 md:p-3 flex flex-col relative grow justify-center gap-2",
         className,
       )}
     >
+      <IconButton
+        className="absolute! top-2 left-2 md:top-3 md:left-3"
+        type="ghost"
+        Icon={ArrowLeft}
+        label="Settings"
+        onClick={onClose}
+      />
       <TextButton
         label={copy.restartConversationButtonLabel}
         Icon={Restart}
--- packages/touchpoint-ui/src/mocks/MockVoice.tsx
+++ packages/touchpoint-ui/src/mocks/MockVoice.tsx
@@ -1,18 +1,19 @@
 /* eslint-disable jsdoc/require-jsdoc */
-import { type FC } from "react";
+import { useState, type FC } from "react";
 import { clsx } from "clsx";
 
 import { ProviderStack } from "../ProviderStack";
 import { LaunchButton } from "../components/ui/LaunchButton";
 import { Main, HeaderContainer } from "../components/Layout";
 import { IconButton } from "../components/ui/IconButton";
-import { Close, Mic } from "../components/ui/Icons";
+import { Close, Mic, Settings as SettingsIcon } from "../components/ui/Icons";
 import { mockConversationHandler, mockTheme, responses } from "./shared";
 import { type WindowSize, type ColorMode } from "../interface";
 import { VoiceIcon } from "../components/FullscreenVoice";
 import { defaultModalities } from "../components/defaultModalities";
 import { VoiceModalities } from "../components/VoiceModalities";
 import { Ripple } from "../components/Ripple";
+import { Settings } from "../components/Settings";
 
 export const MockVoice: FC<{
   embedded: boolean;
@@ -24,6 +25,8 @@ export const MockVoice: FC<{
 }> = (props) => {
   const colorMode = props.colorMode ?? "dark";
   const { isExpanded, onClose, onExpand, windowSize } = props;
+
+  const [settingsOpen, setSettingsOpen] = useState<boolean>(false);
 
   if (!isExpanded) {
     return (
@@ -63,42 +66,76 @@ export const MockVoice: FC<{
             onClick={onClose}
             type="overlay"
           />
+
+          <IconButton
+            Icon={SettingsIcon}
+            label="Settings"
+            onClick={() => {
+              setSettingsOpen(true);
+            }}
+            type="overlay"
+          />
         </HeaderContainer>
-        <div
-          className={clsx(
-            "grow flex flex-col",
-            windowSize === "full" ? "w-full md:max-w-content md:mx-auto" : "",
-          )}
-        >
-          <div className="relative grow">
-            <div className="absolute inset-0 flex items-center justify-center">
-              <VoiceIcon colorMode={colorMode} addRipple className="relative" />
-            </div>
-            <VoiceModalities
-              responses={responses}
-              modalityComponents={defaultModalities}
-              renderedAsOverlay={true}
-              handler={mockConversationHandler}
-              showTranscript={true}
-              className={clsx(
-                "w-full",
-                "absolute inset-0 overflow-auto p-2 md:p-3 space-y-4 z-10",
-                "border-b border-solid border-primary-10",
-              )}
-            />
-          </div>
-          <div className="flex items-center justify-center py-4 flex-none">
-            <div className="w-fit relative">
-              <Ripple className="rounded-inner" />
-              <IconButton
-                Icon={Mic}
-                label="Voice"
-                type={"activated"}
-                onClick={() => {}}
+        {
+          <>
+            {settingsOpen ? (
+              <Settings
+                reset={() => {}}
+                className={clsx(
+                  windowSize === "full"
+                    ? "w-full md:max-w-content md:mx-auto"
+                    : "",
+                )}
+                onClose={() => {
+                  setSettingsOpen(false);
+                }}
+                handler={mockConversationHandler}
               />
-            </div>
-          </div>
-        </div>
+            ) : null}
+            <div
+              className={clsx(
+                "grow flex-col",
+                windowSize === "full"
+                  ? "w-full md:max-w-content md:mx-auto"
+                  : "",
+                settingsOpen ? "hidden" : "flex",
+              )}
+            >
+              <div className="relative grow">
+                <div className="absolute inset-0 flex items-center justify-center">
+                  <VoiceIcon
+                    colorMode={colorMode}
+                    addRipple
+                    className="relative"
+                  />
+                </div>
+                <VoiceModalities
+                  responses={responses}
+                  modalityComponents={defaultModalities}
+                  renderedAsOverlay={true}
+                  handler={mockConversationHandler}
+                  showTranscript={true}
+                  className={clsx(
+                    "w-full",
+                    "absolute inset-0 overflow-auto p-2 md:p-3 space-y-4 z-10",
+                    "border-b border-solid border-primary-10",
+                  )}
+                />
+              </div>
+              <div className="flex items-center justify-center py-4 flex-none">
+                <div className="w-fit relative">
+                  <Ripple className="rounded-inner" />
+                  <IconButton
+                    Icon={Mic}
+                    label="Voice"
+                    type={"activated"}
+                    onClick={() => {}}
+                  />
+                </div>
+              </div>
+            </div>{" "}
+          </>
+        }
       </Main>
     </ProviderStack>
   );
